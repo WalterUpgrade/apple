291ae603f4f6e1e7b82801078ec5af753368e28b
walter upgrade <nbarrera476@gmail.com>
2025-07-15 13:22:03 +0200

    json

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,9 +30,10 @@ const Hero = () => {
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
-        <p id="hero" className="hero-title">iPhone 15 Pro</p>
-        Build the Apple Intelligence
-        <div className="md:w-10/12 w-9/12">
+        <p id="hero" className="hero-title">iPhone 16 Pro</p>
+If you can dream it,
+Iphone can do it.        
+<div className="md:w-10/12 w-9/12">
           <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
             <source src={videoSrc} type="video/mp4" />
           </video>
